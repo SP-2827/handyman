7da16c37beee27404f84b02526f8b545adf13f8c
Mars <shankarprakash32@gmail.com>
2022-06-08 20:14:36 +0530

    changes to upgrade DB

--- chidori/src/main/java/in/handyman/chidori/ChidoriApplication.java
+++ chidori/src/main/java/in/handyman/chidori/ChidoriApplication.java
@@ -1,13 +1,15 @@
 package in.handyman.chidori;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ChidoriApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChidoriApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChidoriApplication.class, args);
+    }
 
 }
--- chidori/src/test/java/in/handyman/chidori/ChidoriApplicationTests.java
+++ chidori/src/test/java/in/handyman/chidori/ChidoriApplicationTests.java
@@ -1,13 +1,17 @@
 package in.handyman.chidori;
 
+import in.handyman.raven.lambda.access.repo.HandymanRepoImpl;
+import in.handyman.raven.lambda.doa.config.SpwCommonConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
 class ChidoriApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        final List<SpwCommonConfig> allCommonConfigs = new HandymanRepoImpl().findAllCommonConfigs();
+        assert allCommonConfigs != null;
+    }
 
 }
